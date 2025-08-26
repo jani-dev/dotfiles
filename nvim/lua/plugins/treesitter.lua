b4b2bc18091908ea79ff048dd80cb94c8577a484
Jani Ahde <janiahde.dev@gmail.com>
2025-08-26 13:35:31 +0300

    feat(nvim): Modify `treesitter.lua` with an attempt to fix installation

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,21 +4,24 @@ end
 
 return {
 	"nvim-treesitter/nvim-treesitter",
-	build = function()
-		require("nvim-treesitter.install").update({ with_sync = true }) ()
-	end,
-	opts = {
-		ensure_installed = {
-			"c_sharp",
-			"lua",
-			"css",
-			"json",
-			"markdown_inline",
-		},
-		sync_install = false,
-		auto_installe = false,
-		highlight = {
-			enable = true,
-		}
-	},
+	build = ":TSUpdate",
+	config = function ()
+		local configs = require("nvim-treesitter.configs")
+
+		configs.setup({
+			ensure_installed = {
+				"c_sharp",
+				"lua",
+				"css",
+				"json",
+				"markdown_inline",
+			},
+			sync_install = false,
+			highlight = {
+				enable = true,
+			},
+			indent = {
+				enable = true,
+			}})
+	end
 }
