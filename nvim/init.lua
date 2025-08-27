413e4d852f23cc5cb474540f526ab7844e1c6281
Jani Ahde <janiahde.dev@gmail.com>
2025-08-27 09:28:14 +0300

    feat(nvim): Change colorscheme to `catppuccin`

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,6 @@
 -- Plugins --
 require("config.lazy")
 
--- Theme & Background
-vim.cmd.colorscheme("unokai")
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
-
 -- Lines
 vim.o.number = true
 vim.o.relativenumber = true
