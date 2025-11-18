c49a4b30bf8de0b9e28669dc4adc75e19081fa95
Jani Ahde <janiahde.dev@gmail.com>
2025-11-18 14:19:35 +0200

    feat(lsp): Modify `roslyn` lsp config through the `init.lua`

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,3 +12,24 @@ vim.o.shiftwidth = 4
 
 -- Behavior
 vim.o.clipboard = "unnamedplus"
+
+vim.lsp.config("roslyn", {
+	on_attach = function(client, bufnr)
+		print("Roslyn started now")
+		vim.lsp.completion.enable(true, client.id, bufnr, {
+			autotrigger = true,
+			convert = function(item)
+				return { abbr = item.label:gsub('%b()', '') }
+			end,
+		})
+	end,
+	settings = {
+		["csharp|completion"] = {
+			dotnet_provide_regex_completions = true,
+			dotnet_show_name_completion_suggestions = true,
+		},
+		["csharp|code_lens"] = {
+			dotnet_enable_references_code_lens = true,
+		}
+	},
+})
